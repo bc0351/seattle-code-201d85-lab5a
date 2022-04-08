df666493c491fa746e57291b7c83340b4c3898fa
2six Dev <dev@twosixgroup.org>
2022-04-08 14:40:09 -0400

    Problem 6 Commit

--- js/app.js
+++ js/app.js
@@ -124,10 +124,11 @@ Test this function by hand in the console to get it working, and when you think 
 let testDynamicArray = [1,2,3,4,5]; //eslint-disable-line
 
 function multiplyAnyArray(dynamicArray) { //eslint-disable-line
-
+    console.log([dynamicArray.reduce(function(elm1, elm2){return multiply(elm1,elm2)[0];}), `The numbers ${dynamicArray.toString()} have a product of ${dynamicArray.reduce(function(elm1, elm2){return multiply(elm1,elm2)[0];})}.`]);
+    return [dynamicArray.reduce(function(elm1, elm2){return multiply(elm1,elm2)[0];}), `The numbers ${dynamicArray.toString()} have a product of ${dynamicArray.reduce(function(elm1, elm2){return multiply(elm1,elm2)[0];})}.`];
 }
 
 // Here is the test for multiplyArray(); uncomment it to run it
-// testMultiplyAnyArray(testDynamicArray);
+testMultiplyAnyArray(testDynamicArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. You're done! Submit the link to the repo following the instructions in Canvas.
